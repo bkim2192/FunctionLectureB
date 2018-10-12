d7be3fe4c1c9a8eb4de79042b86b6453793b0fa0
jhhsxcode <jhhs.xcode@d214.org>
2018-10-12 10:58:31 -0500

    Added UI Elements

--- FunctionLecture/ViewController.swift
+++ FunctionLecture/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfieldOne: UITextField!
+    @IBOutlet weak var textfieldTwo: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
