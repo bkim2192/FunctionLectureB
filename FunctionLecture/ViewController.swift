42be9f399684555634c227dd396ce935641206a0
jhhsxcode <jhhs.xcode@d214.org>
2018-10-12 10:56:40 -0500

    Initial Commit

---
+++ FunctionLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunctionLecture
+//
+//  Created by CS Driver on 10/12/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
